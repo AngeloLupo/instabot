47848ddfe583207b2ac851dcc31b6ad46ab317dc
Angelo Lupo <angelolupo94@gmail.com>
2018-10-17 11:37:49 +0100

    added logic to fetch 2 most recent files

--- check.py
+++ check.py
@@ -1,17 +1,20 @@
 import json
+import os
 
-old_followers_file = open('followers/d2')
-new_followers_file = open('followers/d1')
+
+files = os.listdir('followers')
+files.sort()
+
+old_followers_file = open('followers/{}'.format(files[-2]))
+new_followers_file = open('followers/{}'.format(files[-1]))
 
 old_followers = json.loads(old_followers_file.read())
 new_followers = json.loads(new_followers_file.read())
-
 
 missing_followers = list(set(old_followers) - set(new_followers))
 
 print('missing followers ({})'.format(len(missing_followers)))
 print(missing_followers)
-
 
 new_followers = list(set(new_followers) - set(old_followers))
 
